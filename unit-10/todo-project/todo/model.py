85343d2d64b07311c553147958c832b834dbc57b
couchjanus <couchjanus@gmail.com>
2023-05-25 21:44:15 +0300

    added unit 10

---
+++ unit-10/todo-project/todo/model.py
@@ -0,0 +1,21 @@
+"""This module provides the TODO database functionality"""
+
+from pathlib import Path
+import configparser
+from todo import SUCCESS, DB_WRITE_ERROR
+DEFAULT_DB_FILE_PATH = Path.home().joinpath("."+Path.home().stem + "_todo.json") 
+
+def get_database_path(config_file):
+    """Return the current path to the TODO database."""
+    config_parser = configparser.ConfigParser()
+    config_parser.read(config_file)
+    return Path(config_parser["General"]["database"])
+
+def init_database(db_path):
+    """Create the TODO database"""
+    try:
+        db_path.write_text("[]")
+        return SUCCESS
+    except OSError:
+        return DB_WRITE_ERROR
+    
