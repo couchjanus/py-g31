4612982abe605d694f8d225fb453eaede9b0aa3f
couchjanus <couchjanus@gmail.com>
2023-06-20 21:42:11 +0300

    added unit 17

---
+++ unit-17/tests/test_factory.py
@@ -0,0 +1,8 @@
+from blogger import create_app
+
+def test_config():
+    assert not create_app().testing
+    
+def test_hello(client):
+    response = client.get('/hello')
+    assert response.data == b'Hello, World!' 
